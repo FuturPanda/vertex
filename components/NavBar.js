f58f2e73a1507f4c596bde5eb2ecd868c2889c8e
Futur_Panda <sty.hoareau@gmail.com>
2023-05-07 05:32:28 +0200

    Search icon navbar

---
+++ components/NavBar.js
@@ -0,0 +1,163 @@
+"use client";
+import Tiptap from "../components/Tiptap.js";
+import SideBar from "../components/SideBar.js";
+
+import React from "react";
+import {
+  Navbar,
+  Button,
+  Link,
+  Text,
+  Card,
+  Radio,
+  useTheme,
+  Avatar,
+  Dropdown,
+  Input,
+} from "@nextui-org/react";
+import { SearchIcon } from "./SearchIcon.js";
+
+export default function NavBar() {
+  const { isDark } = useTheme();
+  const collapseItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+  return (
+    <Navbar isBordered variant={"floating"}>
+      <Navbar.Brand>
+        <Text b color="inherit" hideIn="xs">
+          VERTEX
+        </Text>
+      </Navbar.Brand>
+      <Navbar.Content hideIn="xs" variant="highlight">
+        <Navbar.Link href="#">Features</Navbar.Link>
+        <Navbar.Link isActive href="#">
+          Customers
+        </Navbar.Link>
+        <Navbar.Link href="#">Pricing</Navbar.Link>
+        <Navbar.Link href="#">Company</Navbar.Link>
+      </Navbar.Content>
+      {/* SignUp / Login */}
+      {/* <Navbar.Content>
+        <Navbar.Link color="inherit" href="#">
+          Login
+        </Navbar.Link>
+        <Navbar.Item>
+          <Button auto flat as={Link} href="#">
+            Sign Up
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content> */}
+      <Navbar.Content
+        css={{
+          "@xsMax": {
+            w: "100%",
+            jc: "space-between",
+          },
+        }}
+      >
+        <Navbar.Item
+          css={{
+            "@xsMax": {
+              w: "100%",
+              jc: "center",
+            },
+          }}
+        >
+          <Input
+            clearable
+            contentLeft={
+              <SearchIcon fill="var(--nextui-colors-accents6)" size={16} />
+            }
+            contentLeftStyling={false}
+            css={{
+              w: "100%",
+              "@xsMax": {
+                mw: "300px",
+              },
+              "& .nextui-input-content--left": {
+                h: "100%",
+                ml: "$4",
+                dflex: "center",
+              },
+            }}
+            placeholder="Search..."
+          />
+        </Navbar.Item>
+        <Dropdown placement="bottom-right">
+          <Navbar.Item>
+            <Dropdown.Trigger>
+              <Avatar
+                bordered
+                as="button"
+                color="primary"
+                size="md"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </Dropdown.Trigger>
+          </Navbar.Item>
+          <Dropdown.Menu
+            aria-label="User menu actions"
+            color="secondary"
+            onAction={(actionKey) => console.log({ actionKey })}
+          >
+            <Dropdown.Item key="profile" css={{ height: "$18" }}>
+              <Text b color="inherit" css={{ d: "flex" }}>
+                Signed in as
+              </Text>
+              <Text b color="inherit" css={{ d: "flex" }}>
+                zoey@example.com
+              </Text>
+            </Dropdown.Item>
+            <Dropdown.Item key="settings" withDivider>
+              My Settings
+            </Dropdown.Item>
+            <Dropdown.Item key="team_settings">Team Settings</Dropdown.Item>
+            <Dropdown.Item key="analytics" withDivider>
+              Analytics
+            </Dropdown.Item>
+            <Dropdown.Item key="system">System</Dropdown.Item>
+            <Dropdown.Item key="configurations">Configurations</Dropdown.Item>
+            <Dropdown.Item key="help_and_feedback" withDivider>
+              Help & Feedback
+            </Dropdown.Item>
+            <Dropdown.Item key="logout" withDivider color="error">
+              Log Out
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Navbar.Content>
+      <Navbar.Collapse>
+        {collapseItems.map((item, index) => (
+          <Navbar.CollapseItem
+            key={item}
+            activeColor="secondary"
+            css={{
+              color: index === collapseItems.length - 1 ? "$error" : "",
+            }}
+            isActive={index === 2}
+          >
+            <Link
+              color="inherit"
+              css={{
+                minWidth: "100%",
+              }}
+              href="#"
+            >
+              {item}
+            </Link>
+          </Navbar.CollapseItem>
+        ))}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
