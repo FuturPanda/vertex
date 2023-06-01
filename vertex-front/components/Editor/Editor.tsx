4e6bfc867b1e0b63a3de14bd1f9d7aee89159cc7
Hoareau.Steevy <sty.hoareau@gmail.com>
2023-06-01 22:22:12 +0200

    added basic collab

--- vertex-front/components/Editor/Editor.tsx
+++ vertex-front/components/Editor/Editor.tsx
@@ -3,79 +3,105 @@ import {
   CalloutExtension,
   HeadingExtension,
   ItalicExtension,
+  AnnotationExtension,
+  PlaceholderExtension,
+  YjsExtension,
 } from "remirror/extensions";
-import { EditorComponent, Remirror, useRemirror } from "@remirror/react";
-import { Children } from "react";
+import "remirror/styles/all.css";
 
-const contentJson = {
-  type: "doc",
-  content: [
-    {
-      type: "heading",
-      attrs: { level: 1 },
-      content: [{ type: "text", text: "Hello world" }],
-    },
-    {
-      type: "paragraph",
-      content: [
-        { type: "text", text: "Hello " },
-        { type: "text", marks: [{ type: "italic" }], text: "word" },
-      ],
-    },
-  ],
+import React from "react";
+import { WebrtcProvider } from "y-webrtc";
+import * as Y from "yjs";
+import {
+  Remirror,
+  ThemeProvider,
+  useCommands,
+  useActive,
+  EditorComponent,
+  useCurrentSelection,
+  useHelpers,
+  useRemirror,
+} from "@remirror/react";
+
+const ydoc = new Y.Doc();
+// clients connected to the same room-name share document updates
+const provider = new WebrtcProvider("remirror-yjs-demo", ydoc);
+
+const extensions = () => [
+  new AnnotationExtension(),
+  new YjsExtension({ getProvider: () => provider }),
+  new BoldExtension(),
+  new ItalicExtension(),
+  new HeadingExtension(),
+];
+
+const Menu = () => {
+  // const { toggleBold, focus } = useCommands();
+  // const active = useActive();
+  // const { removeAnnotations, addAnnotation, redrawAnnotations } = useCommands();
+  // const { getAnnotationsAt, selectionHasAnnotation } = useHelpers();
+  // const selection = useCurrentSelection();
+
+  return (
+    <>
+      {/* <button
+        onClick={() => {
+          toggleBold();
+          focus();
+        }}
+        style={{ fontWeight: active.bold() ? "bold" : undefined }}
+      >
+        B
+      </button>
+      <button
+        onClick={() => {
+          addAnnotation({ id: `${Date.now()}` });
+          focus();
+        }}
+        disabled={selection.empty}
+      >
+        Add annotation
+      </button>
+      <button
+        onClick={() => {
+          const annotations = getAnnotationsAt(selection.from);
+          removeAnnotations(annotations.map(({ id }) => id));
+          focus();
+        }}
+        disabled={!selectionHasAnnotation()}
+      >
+        Remove annotation(s)
+      </button>
+      <button
+        onClick={() => {
+          redrawAnnotations();
+          focus();
+        }}
+      >
+        Redraw annotation(s)
+      </button> */}
+    </>
+  );
 };
-
-function LoadButton({ initialContent }) {
-  const { setContent } = useRemirrorContext();
-  const handleClick = useCallback(
-    () => console.log("Click " + initialContent),
-    [setContent]
-  );
-
-  return (
-    <button
-      onMouseDown={(event) => event.preventDefault()}
-      onClick={handleClick}
-    >
-      Load
-    </button>
-  );
-}
-
-function SaveButton() {
-  const { getJSON } = useHelpers();
-  const handleClick = useCallback(
-    () => alert(JSON.stringify(getJSON())),
-    [getJSON]
-  );
-  return (
-    <button
-      onMouseDown={(event) => event.preventDefault()}
-      onClick={handleClick}
-    >
-      Save
-    </button>
-  );
-}
-
-const Menu = () => <button onClick={() => alert("TBD")}>B</button>;
 
 export const MyEditor = ({ initialContent }) => {
   const { manager, state } = useRemirror({
-    extensions: () => [
-      new BoldExtension(),
-      new ItalicExtension(),
-      new HeadingExtension(),
-    ],
+    extensions,
+    core: { excludeExtensions: ["history"] },
     content: initialContent,
   });
 
   return (
     <div className="remirror-theme">
-      <Remirror manager={manager} initialContent={state}>
+      <Remirror
+        manager={manager}
+        initialContent={state}
+        autoFocus
+        autoRender="end"
+      >
         {/* The text editor is placed above the menu to make the zIndex easier to manage for popups */}
         <Menu />
-        <EditorComponent />
+        {/* <EditorComponent /> */}
       </Remirror>
     </div>
   );
